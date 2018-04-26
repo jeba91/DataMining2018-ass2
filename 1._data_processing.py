8a88da6d1b0b1f5a3964f5000bd71de03191b775
aleidehoeijmakers <ashoeijmakers@gmail.com>
2018-04-26 09:28:05 +0200

    1

---
+++ 1._data_processing.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import csv
+
+#Load the dataset from CSV
+try:
+    data_all = pd.read_csv("training_set_VU_DM_2014.csv", index_col=0, header=0,  delimiter=",")
+except IOError as e:
+    print('File not found!')
+    raise e
+
+print data_all.head(5)
