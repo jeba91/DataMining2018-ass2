b372bdeb84e1915003c12d176096d39638b95c3b
Jeroen Baars <jeroen.j.baars@gmail.com>
2018-05-07 00:38:02 +0200

    plaatjes

--- 1_data.py
+++ 1_data.py
@@ -76,27 +76,72 @@ except IOError as e:
     print('File not found!')
     raise e
 
-
+func = visualize_functions()
 cols_names = data_all.columns
 
-func = visualize_functions()
 
 # func.heatmap_corr(data_all)
-# # func.QQplot(data_all,cols_names)
-#
-# for col in cols_names:
-#     print(col)
-#     print(data_all[col].describe())
+# data_all.describe().to_csv('describe_data.csv')
 
-print(cols_names)
-# data_all.describe().to_csv('example.csv')
+nandat = data_all.isnull().sum()
+
+plt.bar(cols_names, nandat, 0.25, color="blue")
+plt.xticks(rotation='vertical')
+plt.tight_layout()
+plt.savefig('visualize/nanvalues.png')
 
 
-pd.plotting.scatter_matrix(data_all, diagonal='kde')
+
+plt.close()
+
+range_book = [[0,2],[3,5],[6,11],[12,20],[21,50],[50,500000000000]]
+range_book_str = ['zero to two','three to five','six to eleven','twelve to twenty','twentyone to fifty','fifty plus']
+
+all2 = []
+
+for ran in range_book:
+    count = data_all.loc[(data_all['srch_booking_window'] >= ran[0]) & (data_all['srch_booking_window'] <= ran[1]) & (data_all['booking_bool'] == 1)].shape[0]
+    all2.append(count)
+
+print(range_book_str)
+
+plt.bar(range(len(range_book_str)), all2, 0.25, color="blue")
+plt.xticks(range(len(range_book_str)),range_book_str)
+plt.xticks(rotation='vertical')
+plt.savefig('visualize/book_window.png')
+plt.tight_layout()
 plt.show()
-fig.savefig('visualize/scatter.png')
-plt.close(fig)
 
+
+
+# scores = np.sort(data_all.prop_review_score.unique())
+#
+# print(data_all['booking_bool'])
+#
+# all = []
+#
+# for s in scores:
+#     count = data_all.loc[(data_all['prop_review_score'] == s) & (data_all['booking_bool'] == 1)].shape[0]
+#     all.append(count)
+#
+# print(scores)
+# print(all)
+#
+# y = [3, 10, 7, 5, 3, 4.5, 6, 8.1]
+# N = len(y)
+# x = range(N)
+# width = 0.25
+# plt.bar(scores, all, width, color="blue")
+# plt.savefig('visualize/bookedperstar.png')
+# plt.show()
+
+
+# pd.plotting.scatter_matrix(data_all, diagonal='kde')
+# plt.show()
+# fig.savefig('visualize/scatter.png')
+# plt.close(fig)
+
+#make boxplot, density plot and histogram
 # for col in data_all:
 #     if col == 'date_time':
 #         continue
@@ -107,15 +152,3 @@ plt.close(fig)
 #     data_all.hist(column=col, ax=axes[2])
 #     fig.savefig('visualize/' + col + '.png')
 #     plt.close(fig)    # close the figure
-
-
-
-# import csv
-#
-# with open('persons.csv', 'wb') as csvfile:
-#     filewriter = csv.writer(csvfile, delimiter=',',
-#                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-#     filewriter.writerow(['Name', 'Profession'])
-#     filewriter.writerow(['Derek', 'Software Developer'])
-#     filewriter.writerow(['Steve', 'Software Developer'])
-#     filewriter.writerow(['Paul', 'Manager'])
