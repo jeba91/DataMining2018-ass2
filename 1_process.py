69bf4b93b7a9104ed4bc315f468f8a3decbe6637
Jeroen Baars <jeroen.j.baars@gmail.com>
2018-05-11 22:29:21 +0200

    dataset maken

--- 1_process.py
+++ 1_process.py
@@ -25,31 +25,47 @@ except IOError as e:
     print('File not found!')
     raise e
 
+# data_all = data_all.head(5000)
+
 cols_names = data_all.columns
-
-print(cols_names)
-
-data_all = data_all.drop(['visitor_hist_adr_usd', 'visitor_hist_starrating', 'srch_query_affinity_score', 'prop_starrating'])
 
 comp_names = [['comp1_rate', 'comp1_inv', 'comp1_rate_percent_diff'], ['comp2_rate', 'comp2_inv', 'comp2_rate_percent_diff'],
               ['comp3_rate', 'comp3_inv', 'comp3_rate_percent_diff'], ['comp4_rate', 'comp4_inv', 'comp4_rate_percent_diff'],
               ['comp5_rate', 'comp5_inv', 'comp5_rate_percent_diff'], ['comp6_rate', 'comp6_inv', 'comp6_rate_percent_diff'],
               ['comp7_rate', 'comp7_inv', 'comp7_rate_percent_diff'], ['comp8_rate', 'comp8_inv', 'comp8_rate_percent_diff']]
 
-print(comp_names)
+cheaper = data_all[comp_names[0][0]]
+expensive = data_all[comp_names[0][0]]
+cheapest = data_all[comp_names[0][2]]
+cheaper.iloc[:] = 0
+expensive.iloc[:] = 0
+cheapest.iloc[:] = 0
 
-total = data_all[comp_names[0][0]].where((data_all[comp_names[0][0]] == 4 & data_all[comp_names[0][1]] == 0),0)
+cheap_check = cheapest.values
 
 for comp in comp_names:
-    summing = data_all[comp[0]].where((data_all[comp[0]] == -1 & data_all[comp[1]] == 0),0)
-    total = pd.concat([total,summing],axis=1)
+    conditions1 = [(data_all[comp[0]].values == -1) & (data_all[comp[1]].values == 0)]
+    conditions2 = [(data_all[comp[0]].values == 1) & (data_all[comp[1]].values == 0)]
+    choices1 = [data_all[comp[0]].values]
+    choices2 = [data_all[comp[2]].values]
+    cheap = np.select(conditions1, choices1, default=0)
+    expen = np.select(conditions2, choices1, default=0)
+    max_cheap = np.select(conditions1, choices2, default=0)
+    cheaper = cheaper + cheap
+    expensive = expensive + expen
+    cheap_check = np.maximum(cheap_check, max_cheap)
 
-total = total.sum(axis=1)
+cheapest = cheapest + cheap_check
 
-print(total)
-print(total.loc[total < 0].shape)
-print(data_all.shape)
+data_all['cheaper_comps'] = cheaper
+data_all['expensive_comps'] = expensive
+data_all['cheapest_comp'] = cheapest
 
+for comp in comp_names:
+    print(comp)
+    data_all.drop(comp, axis = 1, inplace = True)
 
-# #save data to pickle file
-# data_all.to_pickle('preprocessed.pkl')
+data_all.drop(['visitor_hist_adr_usd', 'visitor_hist_starrating', 'srch_query_affinity_score', 'gross_bookings_usd'], axis = 1, inplace = True)
+
+#save data to pickle file
+data_all.to_pickle('preprocessed.pkl')
--- 2_data.py
+++ 2_data.py
@@ -66,30 +66,14 @@ class visualize_functions:
         sns.heatmap(corrmat, vmax=.8, square=True);
         plt.xticks(rotation='vertical')
         plt.yticks(rotation='horizontal')
+        plt.tight_layout()
         fig.savefig('visualize/heatmap.png')
 
 
-#Load the dataset from CSV
-try:
-    data_all = pd.read_csv("training_set_VU_DM_2014.csv", index_col=0, header=0,  delimiter=",")
-except IOError as e:
-    print('File not found!')
-    raise e
-
-func = visualize_functions()
-
-# func.heatmap_corr(data_all)
-# data_all.describe().to_csv('describe_data.csv')
+#Read in preprocessed data
+data_all = pd.read_pickle('preprocessed.pkl')
 
 
-comp_names = [['comp1_rate', 'comp1_inv', 'comp1_rate_percent_diff'], ['comp2_rate', 'comp2_inv', 'comp2_rate_percent_diff'],
-              ['comp3_rate', 'comp3_inv', 'comp3_rate_percent_diff'], ['comp4_rate', 'comp4_inv', 'comp4_rate_percent_diff'],
-              ['comp5_rate', 'comp5_inv', 'comp5_rate_percent_diff'], ['comp6_rate', 'comp6_inv', 'comp6_rate_percent_diff'],
-              ['comp7_rate', 'comp7_inv', 'comp7_rate_percent_diff'], ['comp8_rate', 'comp8_inv', 'comp8_rate_percent_diff']]
-
-for com in comp_names:
-    for c in com:
-        data_all = data_all.drop(c)
 
 cols_names = data_all.columns
 nandat = data_all.isnull().sum()
@@ -103,72 +87,18 @@ nandat = [(e/total)*100 for e in nandat]
 
 sorted = range(len(cols_names))
 
-plt.bar(sorted, nandat, color="#3F5D7D", align='edge', width=0.6)
+plt.bar(sorted, nandat, color = "#3F5D7D", align = 'edge', width=0.6)
 plt.ylabel('Percentage NaN values')
 plt.xticks(sorted, cols_names, rotation='vertical')
-
+plt.ylim(0,100)
 plt.tight_layout()
 plt.savefig('visualize/nanvalues.png')
-
 plt.close()
 
-# range_book = [[0,2],[3,5],[6,],[12,20],[21,50],[50,500000000000]]
-# range_book_str = ['zero to two','three to five','six to eleven','twelve to twenty','twentyone to fifty','fifty plus']
+func = visualize_functions()
+func.heatmap_corr(data_all)
 
-# range_book = range(0,100)
-# range_book_str = [str(e) for e in range_book]
-# all2 = []
-#
-# for ran in range_book:
-#     count = data_all.loc[(data_all['srch_booking_window'] == ran) & (data_all['booking_bool'] == 1)].shape[0]
-#     all2.append(count)
-#
-# print(range_book_str)
-#
-# plt.bar(range(len(range_book_str)), all2, 0.25, color="blue")
-# plt.xticks(range(len(range_book_str)),range_book_str)
-# plt.xticks(rotation='vertical')
-# plt.savefig('visualize/book_window.png')
-# plt.tight_layout()
-# plt.show()
-
-
-
-# scores = np.sort(data_all.prop_review_score.unique())
-#
-# print(data_all['booking_bool'])
-#
-# all = []
-#
-# for s in scores:
-#     count = data_all.loc[(data_all['prop_review_score'] == s) & (data_all['booking_bool'] == 1)].shape[0]
-#     all.append(count)
-#
-# print(scores)
-# print(all)
-#
-# y = [3, 10, 7, 5, 3, 4.5, 6, 8.1]
-# N = len(y)
-# x = range(N)
-# width = 0.25
-# plt.bar(scores, all, width, color="blue")
-# plt.savefig('visualize/bookedperstar.png')
-# plt.show()
-
-
-# pd.plotting.scatter_matrix(data_all, diagonal='kde')
-# plt.show()
-# fig.savefig('visualize/scatter.png')
-# plt.close(fig)
-
-#make boxplot, density plot and histogram
-# for col in data_all:
-#     if col == 'date_time':
-#         continue
-#     fig, axes = plt.subplots(1,3, figsize=(25,8))
-#     print(axes)
-#     data_all.boxplot(column=col, ax=axes[0])
-#     data_all[col].plot(kind='kde', ax=axes[1])
-#     data_all.hist(column=col, ax=axes[2])
-#     fig.savefig('visualize/' + col + '.png')
-#     plt.close(fig)    # close the figure
+for name in ['cheaper_comps','expensive_comps','cheapest_comp']:
+    print(data_all[name].describe())
+    print(data_all[name].loc[data_all[name] == 0].shape)
+    print(data_all.shape[0] - data_all[name].loc[data_all[name] == 0].shape[0])
