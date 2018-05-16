fd6cd48c6708b171b994eb1370785a87c5c4cdcd
Jeroen Baars <jeroen.j.baars@gmail.com>
2018-05-16 15:53:22 +0200

    normalized

--- 2_split.py
+++ 2_split.py
@@ -36,14 +36,21 @@ from imblearn.under_sampling import RandomUnderSampler
 #Read in preprocessed data
 data_all = pd.read_pickle('preprocessed2.pkl')
 
-
-
 y_values = data_all['score'].values
 data_all.drop(['score'], axis = 1, inplace = True)
 
 columns = data_all.columns
 columns = columns.append(pd.Index(['PCA1', 'PCA2', 'PCA3']))
 print(columns)
+
+data_all['price_usd'].loc[data_all['price_usd'] > 10000] = 100000
+
+data_all['price_usd'] = (data_all['price_usd']-data_all['price_usd'].min())/(data_all['price_usd'].max()-data_all['price_usd'].min())
+data_all['prop_id'] = (data_all['prop_id']-data_all['prop_id'].min())/(data_all['prop_id'].max()-data_all['prop_id'].min())
+data_all['srch_destination_id'] = (data_all['srch_destination_id']-data_all['srch_destination_id'].min())/(data_all['srch_destination_id'].max()-data_all['srch_destination_id'].min())
+data_all['orig_destination_distance'] = (data_all['orig_destination_distance']-data_all['orig_destination_distance'].min())/(data_all['orig_destination_distance'].max()-data_all['orig_destination_distance'].min())
+
+
 
 x_values = data_all.values
 
@@ -68,59 +75,59 @@ y_train = y_resampled
 
 
 
-print ("hallo")
-#RandomForestEstimator
-estimator = SVC(kernel='linear', C=1, max_iter=5000)
-print(estimator)
-selector = RFECV(estimator, step=1, cv=3, scoring='accuracy', verbose=1)
-selector = selector.fit(x_train, y_train)
-
-from sklearn.externals import joblib
-joblib.dump(selector, 'selector.pkl')
-
-selector = joblib.load('selector.pkl')
-print('Optimal number of features :', selector.n_features_)
-print('Best features :', selector.support_)
-
-indexes = [i for i, x in enumerate(selector.support_) if x]
-
-plt.figure()
-plt.xlabel("Number of features selected")
-plt.ylabel("Cross validation score (min value of MSE)")
-plt.plot(range(1, len(selector.grid_scores_) + 1), selector.grid_scores_)
-plt.show()
-
-
-
-
-# #UNCOMMENT FOR TREE
-# # Build a forest and compute the feature importances
-# forest = ExtraTreesRegressor(n_estimators=250, random_state=0)
-# forest.fit(x_train, y_train)
+# print ("hallo")
+# #RandomForestEstimator
+# estimator = SVC(kernel='linear', C=1, max_iter=5000)
+# print(estimator)
+# selector = RFECV(estimator, step=1, cv=3, scoring='accuracy', verbose=1)
+# selector = selector.fit(x_train, y_train)
 #
-# importances = forest.feature_importances_
-# print(importances)
-# std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
-# indices = np.argsort(importances)[::-1]
+# from sklearn.externals import joblib
+# joblib.dump(selector, 'selector.pkl')
 #
-# # Print the feature ranking
-# print("Feature ranking:")
+# selector = joblib.load('selector.pkl')
+# print('Optimal number of features :', selector.n_features_)
+# print('Best features :', selector.support_)
 #
-# total = 0
+# indexes = [i for i, x in enumerate(selector.support_) if x]
 #
-# for f in range(x_train.shape[1]):
-#     total = total + importances[indices[f]]
-#     print(f + 1, columns[indices[f]-1], indices[f], importances[indices[f]], total)
-#
-# plot_names = [columns[indices[f]-1] for f in range(indices)]
-# print(plot_names)
-#
-# # Plot the feature importances of the forest
 # plt.figure()
-# plt.title("Feature importances")
-# plt.bar(range(x_train.shape[1]), importances[indices], color="r", yerr=std[indices], align="center")
-# plt.xticks(range(x_train.shape[1]), indices)
-# plt.xticks(rotation='vertical')
-# plt.xlim([-1, x_train.shape[1]])
+# plt.xlabel("Number of features selected")
+# plt.ylabel("Cross validation score (min value of MSE)")
+# plt.plot(range(1, len(selector.grid_scores_) + 1), selector.grid_scores_)
 # plt.show()
-# plt.savefig('visualize/tree_importance.png')
+
+
+
+
+#UNCOMMENT FOR TREE
+# Build a forest and compute the feature importances
+forest = ExtraTreesRegressor(n_estimators=250, random_state=0)
+forest.fit(x_train, y_train)
+
+importances = forest.feature_importances_
+print(importances)
+std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
+indices = np.argsort(importances)[::-1]
+
+# Print the feature ranking
+print("Feature ranking:")
+
+total = 0
+
+for f in range(x_train.shape[1]):
+    total = total + importances[indices[f]]
+    print(f + 1, columns[indices[f]-1], indices[f], importances[indices[f]], total)
+
+plot_names = [columns[indices[f]-1] for f in range(len(indices))]
+print(plot_names)
+
+# Plot the feature importances of the forest
+plt.figure()
+plt.title("Feature importances")
+plt.bar(range(x_train.shape[1]), importances[indices], color="r", yerr=std[indices], align="center")
+plt.xticks(range(x_train.shape[1]), plot_names)
+plt.xticks(rotation='vertical')
+plt.xlim([-1, x_train.shape[1]])
+plt.tight_layout()
+plt.savefig('visualize/tree_importance.png')
