9480fb1f384a9cdc265b0e7b4e38067266918859
Jeroen Baars <jeroen.j.baars@gmail.com>
2018-05-18 13:23:30 +0200

    prive

--- 1_PCA.py
+++ 1_PCA.py
@@ -18,7 +18,7 @@ from sklearn import preprocessing
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 from sklearn.feature_selection import RFE
-from sklearn.linear_model import Perceptron
+from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
@@ -28,58 +28,39 @@ from sklearn.datasets import make_friedman1
 from sklearn.feature_selection import RFECV
 from sklearn.metrics import mean_squared_error
 from sklearn.svm import SVR
+from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.svm import SVC
 import pandas as pd
 from sklearn import preprocessing
 from imblearn.under_sampling import RandomUnderSampler
-from sklearn.model_selection import cross_val_score
-from sklearn.datasets import make_blobs
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.tree import DecisionTreeClassifier
-
 
 #Read in preprocessed data
 data_all = pd.read_pickle('preprocessed2.pkl')
 
-y_values = data_all['score'].values
-data_all.drop(['score'], axis = 1, inplace = True)
+data_PCA = data_all.copy(deep=True)
+data_PCA.drop(['score'], axis = 1, inplace = True)
 
-columns = data_all.columns
+columns = data_PCA.columns
 columns = columns.append(pd.Index(['PCA1', 'PCA2', 'PCA3']))
-print(columns)
 
-x_values = data_all.values
+pca_values = data_PCA.values
 
-# Apply the random under-sampling
-rus = RandomUnderSampler(return_indices=True)
-x_resampled, y_resampled, idx_resampled = rus.fit_sample(x_values, y_values)
-
-pca = PCA().fit(x_resampled)
+pca = PCA().fit(pca_values)
 plt.plot(np.cumsum(pca.explained_variance_ratio_))
 plt.xlabel('number of components')
 plt.ylabel('cumulative explained variance')
 plt.savefig('visualize/PCA2.png')
 
-pca = PCA(n_components=2)
-features = pca.fit_transform(x_resampled)
-features_analyse = pca.fit(x_resampled)
+pca = PCA(n_components=3)
+features = pca.fit_transform(pca_values)
+features_analyse = pca.fit(pca_values)
 
-# print(pca.explained_variance_)
-x_train = np.concatenate((x_resampled, features), axis=1)
-y_train = y_resampled
+features = features.transpose()
 
+data_PCA['PCA_1'] = features[0]
+data_PCA['PCA_2'] = features[1]
+data_PCA['PCA_3'] = features[2]
+data_PCA['score'] = data_all['score'].values
 
-from sklearn.model_selection import train_test_split
-X_train, X_test, Y_train, Y_test = train_test_split(x_train, y_train, test_size=0.4, random_state=42)
-
-#print X_test, X_train, Y_test, Y_train
-
-X_train, Y_train = make_blobs(n_samples=50000, n_features=34, centers=3,random_state=0)
-
-clf = ExtraTreesClassifier(n_estimators=1000, max_depth=None, min_samples_split=3, random_state=0)
-
-scores = cross_val_score(clf, X_test, Y_test)
-
-print clf
-print scores
-print scores.mean()
+#save data to pickle file
+data_PCA.to_pickle('preprocessed3.pkl')
--- 2_split.py
+++ 2_split.py
@@ -18,7 +18,7 @@ from sklearn import preprocessing
 from sklearn.feature_selection import SelectKBest
 from sklearn.feature_selection import chi2
 from sklearn.feature_selection import RFE
-from sklearn.linear_model import Perceptron
+from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA
@@ -35,43 +35,26 @@ from sklearn import preprocessing
 from imblearn.under_sampling import RandomUnderSampler
 
 #Read in preprocessed data
-data_all = pd.read_pickle('preprocessed2.pkl')
+data_all = pd.read_pickle('preprocessed3.pkl')
 
 y_values = data_all['score'].values
 data_all.drop(['score'], axis = 1, inplace = True)
-
 columns = data_all.columns
-columns = columns.append(pd.Index(['PCA1', 'PCA2', 'PCA3']))
-print(columns)
-
 x_values = data_all.values
+
 
 # Apply the random under-sampling
 rus = RandomUnderSampler(return_indices=True)
-x_resampled, y_resampled, idx_resampled = rus.fit_sample(x_values, y_values)
+x_train, y_train, idx_resampled = rus.fit_sample(x_values, y_values)
 
-pca = PCA().fit(x_resampled)
-plt.plot(np.cumsum(pca.explained_variance_ratio_))
-plt.xlabel('number of components')
-plt.ylabel('cumulative explained variance')
-plt.savefig('visualize/PCA2.png')
-
-pca = PCA(n_components=3)
-features = pca.fit_transform(x_resampled)
-features_analyse = pca.fit(x_resampled)
-
-# print(pca.explained_variance_)
-x_train = np.concatenate((x_resampled, features), axis=1)
-y_train = y_resampled
-
-
+# x_resampled, y_resampled = x_values, y_values
 
 
 # print ("hallo")
 # #RandomForestEstimator
-# estimator = Perceptron(max_iter=5000)
+# estimator = LinearRegression()
 # print(estimator)
-# selector = RFECV(estimator, step=1, cv=6, scoring='accuracy', verbose=1)
+# selector = RFECV(estimator, step=1, cv=3, scoring='neg_mean_absolute_error', verbose=1)
 # selector = selector.fit(x_train, y_train)
 #
 # from sklearn.externals import joblib
